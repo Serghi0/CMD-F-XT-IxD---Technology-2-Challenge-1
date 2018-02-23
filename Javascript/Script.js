c0da416a8524a2f1460f34eeff29d425818909ef
Serghi0 <s_l_l_@msn.com>
2018-02-23 09:37:07 +0100

    edits

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,9 +1,17 @@
 function generateRandomNumber(){
 number1 = Math.floor(Math.random()*100000000);
+number2= 0;
 if(number1 < 56000000){
 document.getElementById('number').innerHTML= number1;
+
+}
+else{
+document.getElementById('number').innerHTML= number2;
+
 }
 }
+
+
 
 document.getElementsByClassName('menutoggle')[0].onclick = function () {
 	var menu = document.getElementsByTagName('body')[0];
